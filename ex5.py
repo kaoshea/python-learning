edc99bf08b26712a722846ff93690ac906a7e208
Kathleen O'Shea <kathleenoshea@Kathleen-OSheas-MacBook-Pro.local>
2016-03-15 23:06:26 -0500

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,8 +1,8 @@
-my_name = 'Kathleen OShea'
+my_name = 'Jane Doe'
 my_age = 23
-my_height = 70 # inches
-my_weight = 175 # lbs
-my_eyes = 'Blue'
+my_height = 65 # inches
+my_weight = 165 # lbs
+my_eyes = 'Green'
 my_teeth = 'White'
 my_hair = 'Brown'
 
@@ -11,6 +11,6 @@ print "She's %d inches tall." % my_height
 print "She's %d pounds heavy." % my_weight
 print "Actually, that's not too heavy."
 print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "Her teeth are usually %s depending on coffee/tea intake." % my_teeth
+print "Her teeth are usually %s depending on the coffee." % my_teeth
 
 print "If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
